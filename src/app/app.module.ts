f69308bc0169deb0b1ab4774b793930bafbec19c
JohnAzmy <johnazmy@yahoo.com>
2017-09-24 14:22:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VisionComponent } from './vision/vision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VisionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path:'vision/addnew', component: VisionComponent
+      },
+      {
+        path: '', component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
